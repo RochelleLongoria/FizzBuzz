edc708f8875ff1c1b127806848013fd8919f48a8
Rochelle Longoria <rochellelong95@gmail.com>
2022-02-28 17:14:45 -0700

    commit

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+    for( var i = 0; i >= 100; i++) {
+        if( i%3 == 0)
+
+        console.log("Fizz")
+
+        else if (i%5 == 0)
+
+        console.log("Buzz")
+
+        else (i%3 && i%5 == 0)
+
+        console.log("Fizz"+"Buzz")
+
+
+    }
